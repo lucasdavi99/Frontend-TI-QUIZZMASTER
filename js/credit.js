54cc8031a1b8d6be72c6b09d6a2af6a8a339b378
Lucas Davi <lucasdavi99@hotmail.com>
2023-03-13 14:28:43 -0300

    Quinto commit
    - Adicionado o logo no ícone do navegador
    
             Em breve mais atualizações

--- js/credit.js
+++ js/credit.js
@@ -1,3 +1,4 @@
+//Opção de pular os creditos
 setTimeout(function() {
   document.getElementById('back-home').style.display = 'block';
 }, 10000); //Segundos para aparecer o botão.
@@ -5,3 +6,5 @@ setTimeout(function() {
 document.getElementById('back-home').addEventListener('click', function() {
   window.location.href = 'index.html'; // Página que será redirecionado.
 });
+
+
