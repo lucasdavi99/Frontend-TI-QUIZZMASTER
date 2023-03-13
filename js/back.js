46a74290138f1ac7fd46e643a322d55faf35861c
Lucas Davi <lucasdavi99@hotmail.com>
2023-03-13 00:22:28 -0300

    quarto comit
    - Adicionado botão de pular os créditos
    
    - Adicionado tela de cadastro e login
    
             Em breve novas atualizações

---
+++ js/back.js
@@ -0,0 +1,7 @@
+setTimeout(function() {
+  document.getElementById('back-home').style.display = 'block';
+}, 10000); //Segundos para aparecer o botão.
+
+document.getElementById('back-home').addEventListener('click', function() {
+  window.location.href = 'index.html'; // Página que será redirecionado.
+});
