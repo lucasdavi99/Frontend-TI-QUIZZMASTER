4f6ecd47e971f0e1f28ba5904b952ad6689e54c5
Lucas Davi <lucasdavi99@hotmail.com>
2023-05-19 01:00:51 -0300

    14° commit
    Adicionado o sistema de pergunta e resposta

--- js/questions.js
+++ js/questions.js
@@ -1,51 +1,72 @@
-const apiUrl = 'https://quizapi.io/api/v1/questions';
-const apiKey = 'hTazyFwBaqDrXelIIggjAtGtjNl3piVhpk2GZtzG';
-const category = 'linux';
-const difficulty = 'Hard';
-const limit = 1;
-
-const requestUrl = `${apiUrl}?apiKey=${apiKey}&category=${category}&difficulty=${difficulty}&limit=${limit}`;
-
-fetch(requestUrl)
+let pontuacao = 0;
+fetch('js/arquivo.json')
   .then(response => response.json())
   .then(data => {
-    const answers = data[0].answers; // assume que as respostas estão na primeira questão
-    document.getElementById('answer_a').textContent = answers.answer_a;
-    document.getElementById('answer_b').textContent = answers.answer_b;
-    document.getElementById('answer_c').textContent = answers.answer_c;
-    document.getElementById('answer_d').textContent = answers.answer_d;
+    let questions = data;
+    let shuffledQuestions = shuffleArray(questions); // Embaralha a ordem das perguntas
+    let currentQuestionIndex = 0;
+    const totalQuestions = shuffledQuestions.length;
 
-    const pergunta = data [0].question;
-    const question = data [0];
+    function shuffleArray(array) {
+      // Função para embaralhar um array utilizando o algoritmo Fisher-Yates
+      for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+      }
+      return array;
+    }
 
-    console.log("pergunta = " + pergunta)
-    console.log(question)
+    function showQuestion(index) {
+      const question = shuffledQuestions[index];
+      const shuffledAnswers = shuffleArray(question.respostas); // Embaralha a ordem das respostas
+      document.getElementById('answer_a').textContent = shuffledAnswers[0];
+      document.getElementById('answer_b').textContent = shuffledAnswers[1];
+      document.getElementById('answer_c').textContent = shuffledAnswers[2];
+      document.getElementById('answer_d').textContent = shuffledAnswers[3];
+      document.getElementById('Questao').textContent = question.pergunta;
+      return question;
+    }
 
-    document.getElementById('Questao').textContent = pergunta;
+    function handleAnswerSelection(userAnswer) {
+      const question = shuffledQuestions[currentQuestionIndex];
+      const correctAnswer = question.resposta_correta;
 
+      console.log("user = " + userAnswer);
+      console.log("resposta certa = " + correctAnswer);
 
+      if (userAnswer === correctAnswer) {
+        console.log('Resposta correta! Você acertou!');
+        currentQuestionIndex++;
+        pontuacao++;
+      } else {
+        console.log('Resposta incorreta! Você errou!');
+        console.log('Você não é inteligente!');
+        alert('Sua pontuação foi ' + pontuacao);
 
-    var correctAnswer = question.correct_answer;
-    console.log(question.correct_answer);
-    
-    
+        currentQuestionIndex = 0; // Volta para a primeira questão se errar
+      }
+
+      if (currentQuestionIndex < totalQuestions) {
+        showQuestion(currentQuestionIndex);
+      } else {
+        console.log('Fim do questionário. Parabéns!');
+        alert('Sua pontuação foi ' + pontuacao);
+        // Ou exibir a mensagem de fim do questionário em um elemento HTML
+      }
+    }
+
+    showQuestion(currentQuestionIndex);
+
     const answerElements = document.querySelectorAll('.box');
 
-
-    answerElements.forEach(element => {
+    answerElements.forEach((element, index) => {
       element.addEventListener('click', () => {
-        // const userAnswer = element.textContent.toLowerCase();
-        var userAnswer = element.id
-        console.log("user = " + element.id)
-        console.log("resposta certa = " + correctAnswer)
-        if (userAnswer == correctAnswer) {
-          console.log('Resposta correta! Você acertou!');
-          // ou exibir a mensagem em um elemento HTML:
-        } else {
-          console.log('Resposta incorreta!, burro pra klr');
-          // faça algo para mostrar a resposta correta e/ou permitir que o usuário tente novamente
-        }
+        var userAnswer = shuffledQuestions[currentQuestionIndex].respostas[index];
+        handleAnswerSelection(userAnswer);
       });
     });
   })
   .catch(error => console.error(error));
+
+
+  
