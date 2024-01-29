156b384e1a94f497b7f67535466958333735113c
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2024-01-29 14:09:20 -0300

    implementando API

--- js/questions.js
+++ js/questions.js
@@ -1,64 +1,62 @@
 let pontuacao = 0;
-fetch('js/arquivo.json')
-  .then(response => response.json())
-  .then(data => {
-    let questions = data;
-    let shuffledQuestions = shuffleArray(questions); // Embaralha a ordem das perguntas
-    let currentQuestionIndex = 0;
-    const totalQuestions = shuffledQuestions.length;
+let currentQuestionIndex = 0;
+let shuffledQuestions = [];
 
-    function shuffleArray(array) {
-      // Função para embaralhar um array utilizando o algoritmo Fisher-Yates
-      for (let i = array.length - 1; i > 0; i--) {
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
-      }
-      return array;
     }
+    return array;
+}
 
-    function showQuestion(index) {
-      const question = shuffledQuestions[index];
-      const shuffledAnswers = shuffleArray(question.respostas); // Embaralha a ordem das respostas
-      document.getElementById('answer_a').textContent = shuffledAnswers[0];
-      document.getElementById('answer_b').textContent = shuffledAnswers[1];
-      document.getElementById('answer_c').textContent = shuffledAnswers[2];
-      document.getElementById('answer_d').textContent = shuffledAnswers[3];
-      document.getElementById('Questao').textContent = question.pergunta;
-      return question;
+function loadQuestionsFromAPI() {
+    fetch('http://localhost:8080/api/questions')
+        .then(response => response.json())
+        .then(data => {
+            shuffledQuestions = shuffleArray(data);
+            showQuestion(currentQuestionIndex);
+        })
+        .catch(error => console.error('Erro ao buscar as perguntas:', error));
+}
+
+function showQuestion(index) {
+    if (index < shuffledQuestions.length) {
+        const question = shuffledQuestions[index];
+        const shuffledAnswers = shuffleArray(question.answers);
+        document.getElementById('answer_a').textContent = shuffledAnswers[0].content;
+        document.getElementById('answer_b').textContent = shuffledAnswers[1].content;
+        document.getElementById('answer_c').textContent = shuffledAnswers[2].content;
+        document.getElementById('answer_d').textContent = shuffledAnswers[3].content;
+        document.getElementById('Questao').textContent = question.content;
+    } else {
+        showModal('Fim do questionário. Parabéns! Sua pontuação foi ' + pontuacao);
     }
+}
 
-    function handleAnswerSelection(userAnswer) {
-      const question = shuffledQuestions[currentQuestionIndex];
-      const correctAnswer = question.resposta_correta;
+function handleAnswerSelection(selectedIndex) {
+    const question = shuffledQuestions[currentQuestionIndex];
+    const correctAnswerIndex = question.answers.findIndex(answer => answer.isCorrect);
 
-      if (userAnswer === correctAnswer) {
+    if (selectedIndex === correctAnswerIndex) {
+        pontuacao += 100;
         currentQuestionIndex++;
-        pontuacao+=100;
-      } else {
-        showModal('Sua pontuação foi ' + pontuacao); //+ ' |||' +'  Resposta certa:' + correctAnswer);
-        currentQuestionIndex = 0; // Volta para a primeira questão se errar
-      }
-
-      if (currentQuestionIndex < totalQuestions) {
-        showQuestion(currentQuestionIndex);
-      } else {
-        showModal('Fim do questionário. Parabéns!' + '|||' + 'Sua pontuação foi ' + pontuacao);
-        // Ou exibir a mensagem de fim do questionário em um elemento HTML
-      }
+    } else {
+        showModal('Sua pontuação foi ' + pontuacao);
+        currentQuestionIndex = 0;
+        pontuacao = 0;
     }
 
     showQuestion(currentQuestionIndex);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loadQuestionsFromAPI();
 
     const answerElements = document.querySelectorAll('.box');
-
     answerElements.forEach((element, index) => {
-      element.addEventListener('click', () => {
-        var userAnswer = shuffledQuestions[currentQuestionIndex].respostas[index];
-        handleAnswerSelection(userAnswer);
-      });
+        element.addEventListener('click', () => {
+            handleAnswerSelection(index);
+        });
     });
-  })
-  .catch(error => console.error(error));
-
-
-  
+});
